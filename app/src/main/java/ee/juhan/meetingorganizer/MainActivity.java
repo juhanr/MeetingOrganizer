956fa59e58fa2ad661db16778306effb4363d66c
juhanr <juhan@risti.eu>
2014-12-17 16:55:13 +0200

    Initial commit

---
+++ app/src/main/java/ee/juhan/meetingorganizer/MainActivity.java
@@ -0,0 +1,42 @@
+package ee.juhan.meetingorganizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import ee.juhan.meetingorganizer.fragments.CustomMapFragment;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new CustomMapFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/ee/juhan/meetingorganizer/fragments/CustomMapFragment.java
@@ -0,0 +1,7 @@
+package ee.juhan.meetingorganizer.fragments;
+
+import android.app.Fragment;
+
+public class CustomMapFragment extends Fragment {
+
+}
